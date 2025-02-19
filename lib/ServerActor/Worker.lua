5d03828b66e56817d0503db370d3f212cdac594f
Joseph <joseph42@live.co.uk>
2025-02-18 22:26:52 -0500

    Add working version

---
+++ lib/ServerActor/Worker.lua
@@ -0,0 +1,69 @@
+local actor = script:GetActor()
+
+local connections = {}
+local preparedRunnable = nil
+local bindableEvent = nil
+
+local tasks = {}
+
+local destroyed = false
+
+local function add(connection: RBXScriptConnection)
+  table.insert(connections, connection)
+end
+
+add(actor:BindToMessage("Parallel:Init", function(bindable: BindableEvent, runnable: (...any?) -> any?)
+  assert(not destroyed, "Parallel destroyed")
+  bindableEvent = bindable
+  preparedRunnable = runnable
+end))
+
+add(actor:BindToMessage("Parallel:Run", function(...: any?)
+  assert(preparedRunnable, "Parallel not initialized")
+  assert(not destroyed, "Parallel destroyed")
+  preparedRunnable(...)
+end))
+
+add(actor:BindToMessage("Parallel:SubmitTask", function(uuid: string, ...: any?)
+  assert(preparedRunnable, "Parallel not initialized")
+  assert(not destroyed, "Parallel destroyed")
+  local args = {...}
+
+  if tasks[uuid] ~= nil then
+    task.cancel(tasks[uuid])
+    tasks[uuid] = nil
+  end
+  tasks[uuid] = task.spawn(function()
+    bindableEvent:Fire(uuid, preparedRunnable(table.unpack(args)))
+  end)
+end))
+
+add(actor:BindToMessage("Parallel:CancelTask", function(uuid: string)
+  assert(preparedRunnable, "Parallel not initialized")
+  assert(not destroyed, "Parallel destroyed")
+  if tasks[uuid] ~= nil then
+    task.cancel(tasks[uuid])
+    tasks[uuid] = nil
+  end
+end))
+
+add(actor:BindToMessage("Parallel:Destroy", function()
+  assert(preparedRunnable, "Parallel not initialized")
+  assert(not destroyed, "Parallel destroyed")
+  destroyed = true
+
+  for _, connection in connections do
+    connection:Disconnect()
+  end
+  connections = {}
+
+  for _, t in tasks do
+    task.cancel(t)
+  end
+  tasks = {}
+
+  preparedRunnable = nil
+  bindableEvent = nil
+end))
+
+return nil
